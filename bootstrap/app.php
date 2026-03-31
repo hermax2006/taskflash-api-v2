a1f9c8dacbabf4fdbb4efc3366f1a21e7fb2437b
root <root@KGH-inconnu.localdomain>
2026-03-31 08:41:17 +0000

    fix nixpacks no frontendd

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->prepend(HandleCors::class);
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,13 @@
+
+
+
 <?php
 
-return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 86400,
-    'supports_credentials' => false,
-];
+'paths' => ['api/*', 'sanctum/csrf-cookie'],
+'allowed_methods' => ['*'],
+'allowed_origins' => ['*'], // On autorise TOUT pour le test
+'allowed_origins_patterns' => [],
+'allowed_headers' => ['*'],
+'exposed_headers' => [],
+'max_age' => 0,
+'supports_credentials' => false, // OBLIGATOIRE si allowed_origins est '*'
