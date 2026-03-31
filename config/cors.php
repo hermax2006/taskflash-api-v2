4f52918037ed9000846952f481d0272fe23cd54b
root <root@KGH-inconnu.localdomain>
2026-03-31 08:43:32 +0000

    fix nixpacks no frontendd

--- config/cors.php
+++ config/cors.php
@@ -3,11 +3,15 @@
 
 <?php
 
-'paths' => ['api/*', 'sanctum/csrf-cookie'],
-'allowed_methods' => ['*'],
-'allowed_origins' => ['*'], // On autorise TOUT pour le test
-'allowed_origins_patterns' => [],
-'allowed_headers' => ['*'],
-'exposed_headers' => [],
-'max_age' => 0,
-'supports_credentials' => false, // OBLIGATOIRE si allowed_origins est '*'
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'], 
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
