7fe24b840828d01d4262a3120394c6d5011f5c95
root <root@KGH-inconnu.localdomain>
2026-03-31 09:09:28 +0000

    fix nixpacks no frontendd

--- config/cors.php
+++ config/cors.php
@@ -3,15 +3,15 @@
 
 <?php
 
-<?php
-
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], 
+    'allowed_origins' => [
+    'https://taskflash-djlwpb4rt-hermax2006s-projects.vercel.app', 
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => false,
+    'max_age' => 86400,
+    'supports_credentials' => true,
 ];
