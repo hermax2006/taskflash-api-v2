fc6e6dcad2ec20b4d433268f4495361e77e23d71
root <root@KGH-inconnu.localdomain>
2026-04-01 08:50:20 +0000

    fix cors allow all origins

--- config/cors.php
+++ config/cors.php
@@ -1,29 +1,12 @@
-
-
-
 <?php
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-    'https://taskflash-djlwpb4rt-hermax2006s-projects.vercel.app', 
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 86400,
-    'supports_credentials' => true,
-];
-<?php
-
-return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://taskflash.vercel.app'],
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 86400,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
